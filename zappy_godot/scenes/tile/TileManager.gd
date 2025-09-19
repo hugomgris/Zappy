ff023df2d5cf5967e22146a2973a1068958da09f
Hugo Munoz Gris <hmunoz-g@car13s2.42barcelona.com>
2025-09-19 14:51:41 +0200

    new enviro

---
+++ zappy_godot/scenes/tile/TileManager.gd
@@ -0,0 +1,10 @@
+extends StaticBody3D
+
+@onready var marker_3d: Marker3D = %Marker3D
+
+func _ready() -> void:
+	const resource = preload("res://scenes/tile/resource.tscn")
+	var new_resource = resource.instantiate()
+	%Marker3D.add_child(new_resource)
+	
+	new_resource.global_transform = %Marker3D.global_transform
